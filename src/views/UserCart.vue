a6cf1c553bb00751d450fd854162f8c66afdb115
user638726 <lienjackson35@gmail.com>
2025-08-28 14:56:01 +0800

    當購物車為零時，購物車清單與訂單不會顯示

--- src/views/UserCart.vue
+++ src/views/UserCart.vue
@@ -19,176 +19,188 @@
   </div>
 
   <div class="container" style="padding-top: 70px">
-    <div class="row mt-4">
-      <!-- 購物車列表 -->
-      <div class="col-md-5 mx-auto">
-        <div class="sticky-top">
-          <table class="table align-middle">
-            <thead>
-              <tr>
-                <th></th>
-                <th>品名</th>
-                <th style="width: 110px">數量</th>
-                <th class="text-end">單價</th>
-              </tr>
-            </thead>
-            <tbody>
-              <template v-if="cart.carts">
-                <tr v-for="item in cart.carts" :key="item.id">
-                  <td>
-                    <button
-                      type="button"
-                      class="btn btn-outline-danger btn-sm"
-                      :disabled="status.loadingItem === item.id"
-                      @click="removeCartItem(item.id)"
-                    >
-                      <i class="bi bi-x"></i>
-                    </button>
-                  </td>
-                  <td>
-                    {{ item.product.title }}
-                    <div class="text-success" v-if="item.coupon">
-                      已套用優惠券
-                    </div>
-                  </td>
-                  <td>
-                    <div class="input-group input-group-sm">
-                      <input
-                        type="number"
-                        class="form-control"
-                        min="1"
-                        :disabled="item.id === status.loadingItem"
-                        @change="updateCart(item)"
-                        v-model.number="item.qty"
-                      />
-                      <div class="input-group-text">
-                        / {{ item.product.unit }}
+    <div v-if="cart.carts && cart.carts.length === 0" class="text-center my-5">
+      <h4 class="text-muted mb-3">您的購物車是空的</h4>
+      <router-link to="/frontproducts" class="btn btn-dark">
+        前往產品頁選購
+      </router-link>
+    </div>
+    <div v-else>
+      <div class="row mt-4">
+        <!-- 購物車列表 -->
+        <div class="col-md-5 mx-auto">
+          <div class="sticky-top">
+            <table class="table align-middle">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>品名</th>
+                  <th style="width: 110px">數量</th>
+                  <th class="text-end">單價</th>
+                </tr>
+              </thead>
+              <tbody>
+                <template v-if="cart.carts">
+                  <tr v-for="item in cart.carts" :key="item.id">
+                    <td>
+                      <button
+                        type="button"
+                        class="btn btn-outline-danger btn-sm"
+                        :disabled="status.loadingItem === item.id"
+                        @click="removeCartItem(item.id)"
+                      >
+                        <i class="bi bi-x"></i>
+                      </button>
+                    </td>
+                    <td>
+                      {{ item.product.title }}
+                      <div class="text-success" v-if="item.coupon">
+                        已套用優惠券
                       </div>
-                    </div>
-                  </td>
+                    </td>
+                    <td>
+                      <div class="input-group input-group-sm">
+                        <input
+                          type="number"
+                          class="form-control"
+                          min="1"
+                          :disabled="item.id === status.loadingItem"
+                          @change="updateCart(item)"
+                          v-model.number="item.qty"
+                        />
+                        <div class="input-group-text">
+                          / {{ item.product.unit }}
+                        </div>
+                      </div>
+                    </td>
+                    <td class="text-end">
+                      <small
+                        v-if="cart.final_total !== cart.total"
+                        class="text-success"
+                        >折扣價：</small
+                      >
+                      NT$ {{ $filters.currency(item.final_total) }}
+                    </td>
+                  </tr>
+                </template>
+              </tbody>
+              <tfoot>
+                <tr>
+                  <td colspan="3" class="text-end">總計</td>
                   <td class="text-end">
-                    <small
-                      v-if="cart.final_total !== cart.total"
-                      class="text-success"
-                      >折扣價：</small
-                    >
-                    NT$ {{ $filters.currency(item.final_total) }}
+                    NT$ {{ $filters.currency(cart.total) }}
                   </td>
                 </tr>
-              </template>
-            </tbody>
-            <tfoot>
-              <tr>
-                <td colspan="3" class="text-end">總計</td>
-                <td class="text-end">
-                  NT$ {{ $filters.currency(cart.total) }}
-                </td>
-              </tr>
-              <tr v-if="cart.final_total !== cart.total">
-                <td colspan="3" class="text-end text-success">折扣價</td>
-                <td class="text-end text-success">
-                  NT$ {{ $filters.currency(cart.final_total) }}
-                </td>
-              </tr>
-            </tfoot>
-          </table>
-          <div class="input-group mb-3 input-group-sm">
-            <input
-              type="text"
-              class="form-control"
-              v-model="coupon_code"
-              placeholder="請輸入優惠碼:GOATM"
-            />
-            <div class="input-group-append">
-              <button
-                class="btn btn-outline-secondary"
-                type="button"
-                @click="addCouponCode"
-              >
-                套用優惠碼
-              </button>
+                <tr v-if="cart.final_total !== cart.total">
+                  <td colspan="3" class="text-end text-success">折扣價</td>
+                  <td class="text-end text-success">
+                    NT$ {{ $filters.currency(cart.final_total) }}
+                  </td>
+                </tr>
+              </tfoot>
+            </table>
+            <div class="input-group mb-3 input-group-sm">
+              <input
+                type="text"
+                class="form-control"
+                v-model="coupon_code"
+                placeholder="請輸入優惠碼:GOATM"
+              />
+              <div class="input-group-append">
+                <button
+                  class="btn btn-outline-secondary"
+                  type="button"
+                  @click="addCouponCode"
+                >
+                  套用優惠碼
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <div class="my-5 row justify-content-center">
-      <Form class="col-md-6" v-slot="{ errors }" @submit="createOrder">
-        <div class="mb-3">
-          <label for="email" class="form-label">Email</label>
-          <Field
-            id="email"
-            name="email"
-            type="email"
-            class="form-control"
-            :class="{ 'is-invalid': errors['email'] }"
-            placeholder="請輸入 Email"
-            rules="email|required"
-            v-model="form.user.email"
-          ></Field>
-          <ErrorMessage name="email" class="invalid-feedback"></ErrorMessage>
-        </div>
 
-        <div class="mb-3">
-          <label for="name" class="form-label">收件人姓名</label>
-          <Field
-            id="name"
-            name="name"
-            type="text"
-            class="form-control"
-            :class="{ 'is-invalid': errors['name'] }"
-            placeholder="請輸入姓名"
-            rules="required"
-            v-model="form.user.name"
-          ></Field>
-          <ErrorMessage name="name" class="invalid-feedback"></ErrorMessage>
-        </div>
+      <div class="my-5 row justify-content-center">
+        <Form class="col-md-6" v-slot="{ errors }" @submit="createOrder">
+          <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <Field
+              id="email"
+              name="email"
+              type="email"
+              class="form-control"
+              :class="{ 'is-invalid': errors['email'] }"
+              placeholder="請輸入 Email"
+              rules="email|required"
+              v-model="form.user.email"
+            ></Field>
+            <ErrorMessage name="email" class="invalid-feedback"></ErrorMessage>
+          </div>
 
-        <div class="mb-3">
-          <label for="tel" class="form-label">收件人電話</label>
-          <Field
-            id="tel"
-            name="tel"
-            type="tel"
-            class="form-control"
-            :class="{ 'is-invalid': errors['tel'] }"
-            placeholder="請輸入電話"
-            rules="required"
-            v-model="form.user.tel"
-          ></Field>
-          <ErrorMessage name="tel" class="invalid-feedback"></ErrorMessage>
-        </div>
+          <div class="mb-3">
+            <label for="name" class="form-label">收件人姓名</label>
+            <Field
+              id="name"
+              name="name"
+              type="text"
+              class="form-control"
+              :class="{ 'is-invalid': errors['name'] }"
+              placeholder="請輸入姓名"
+              rules="required"
+              v-model="form.user.name"
+            ></Field>
+            <ErrorMessage name="name" class="invalid-feedback"></ErrorMessage>
+          </div>
 
-        <div class="mb-3">
-          <label for="address" class="form-label">收件人地址</label>
-          <Field
-            id="address"
-            name="address"
-            type="text"
-            class="form-control"
-            :class="{ 'is-invalid': errors['address'] }"
-            placeholder="請輸入地址"
-            rules="required"
-            v-model="form.user.address"
-          ></Field>
-          <ErrorMessage name="address" class="invalid-feedback"></ErrorMessage>
-        </div>
+          <div class="mb-3">
+            <label for="tel" class="form-label">收件人電話</label>
+            <Field
+              id="tel"
+              name="tel"
+              type="tel"
+              class="form-control"
+              :class="{ 'is-invalid': errors['tel'] }"
+              placeholder="請輸入電話"
+              rules="required"
+              v-model="form.user.tel"
+            ></Field>
+            <ErrorMessage name="tel" class="invalid-feedback"></ErrorMessage>
+          </div>
 
-        <div class="mb-3">
-          <label for="message" class="form-label">留言[選填]</label>
-          <textarea
-            name=""
-            id="message"
-            class="form-control"
-            cols="30"
-            rows="10"
-            v-model="form.message"
-          ></textarea>
-        </div>
-        <div class="text-end">
-          <button class="btn btn-dark">送出訂單</button>
-        </div>
-      </Form>
+          <div class="mb-3">
+            <label for="address" class="form-label">收件人地址</label>
+            <Field
+              id="address"
+              name="address"
+              type="text"
+              class="form-control"
+              :class="{ 'is-invalid': errors['address'] }"
+              placeholder="請輸入地址"
+              rules="required"
+              v-model="form.user.address"
+            ></Field>
+            <ErrorMessage
+              name="address"
+              class="invalid-feedback"
+            ></ErrorMessage>
+          </div>
+
+          <div class="mb-3">
+            <label for="message" class="form-label">留言[選填]</label>
+            <textarea
+              name=""
+              id="message"
+              class="form-control"
+              cols="30"
+              rows="10"
+              v-model="form.message"
+            ></textarea>
+          </div>
+          <div class="text-end">
+            <button class="btn btn-dark">送出訂單</button>
+          </div>
+        </Form>
+      </div>
     </div>
   </div>
   <footer class="footer-fixed bg-dark text-white text-center py-3">
