ccf74794d9d2b98abecd49bf24ad4289a647e4aa
user638726 <lienjackson35@gmail.com>
2025-09-21 11:23:36 +0800

    更換Json檔

--- src/views/News.vue
+++ src/views/News.vue
@@ -122,7 +122,7 @@ export default {
         loadingItem: "",
       },
       showCartPreview: false,
-      files: ["nba_6502.json", "nba_6503.json"],
+      files: ["nba_6499.json", "nba_6500.json"],
       posts: [],
       error: null,
     };
